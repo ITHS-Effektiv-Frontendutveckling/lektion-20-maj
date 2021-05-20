346853ae656f12f2121d5009d866938a88f48cc0
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2021-05-20 15:09:38 +0200

    Added library with router

---
+++ library-with-router/src/components/Book.js
@@ -0,0 +1,22 @@
+import './Book.css';
+import { useHistory } from 'react-router-dom';
+
+function Book({ book }) {
+  const history = useHistory();
+
+  return (
+    <article className="book" onClick={ () => { history.push(`/book/${book.id}`) }}
+    style={{ backgroundColor: book.color }}>
+      <section className="bg"></section>
+        <section className="content">
+          <aside></aside>
+          <section className="about">
+            <h2>{ book.title }</h2>
+            <h3>{ book.author }</h3>
+          </section>
+        </section>
+    </article>
+  )
+}
+
+export default Book;
